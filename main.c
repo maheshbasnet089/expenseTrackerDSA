cd88333b5c1e750ac1a8063ba11de8d50d2bf1d1
maheshbasnet089 <basnetmanish089@gmail.com>
2023-07-12 21:32:12 +0545

    initialize project

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+// show the list in the terminal function here
+
+
+
+//add expense function
+
+
+//add income function
+
+
+//subtract expense from income function here
+
+
+// calcute the total remaining amount function here
+
+
+
+// take the numbers and give the output in % function here 
+
+// listing the incomeExpenseReport function here
 
 void main()
 {
